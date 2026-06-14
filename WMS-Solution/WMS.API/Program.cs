22413f4b6f8f440b028b4f5a3dc6211d780f0452
Dishika <dishikaa11@gmail.com>
2026-06-14 23:57:42 +0530

    Fix CORS for Azure Static Web App

--- WMS-Solution/WMS.API/Program.cs
+++ WMS-Solution/WMS.API/Program.cs
@@ -136,7 +136,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:4200")
+                .WithOrigins("http://localhost:4200, https://calm-smoke-0e86c6a00.7.azurestaticapps.net")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
